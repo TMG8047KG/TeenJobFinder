b545d8ba5012b452aedddbd87693d0726561207f
lookingforout <spycrabdad@gmail.com>
2024-08-20 16:46:44 +0300

    Main Page Base (Non Official)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,69 @@
 <x-layout>
     <x-nav/>
-    Home
+
+    <div class="px-3 rounded-lg  flex flex-col w-full">
+        <h4 class="text-black text-xl font-semibold  leading-tight truncate">Find your job
+        </h4>
+    </div>
+    <div class="flex items-center justify-between mt-3 px-3 z-10">
+        <div class="relative w-full">
+            <input type="text" class="bg-purple-white shadow rounded-xl border-0 p-3 w-full" placeholder="Search a job...">
+            <div class="absolute top-0 right-0 p-4 pr-3 text-purple-lighter">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                </svg>
+            </div>
+        </div>
+    </div>
+    <div class=" p-3 space-y-4 z-0">
+        <h4 class="font-semibold">Recommended Jobs</h4>
+        <div class="grid m-0  grid-cols-2  space-x-4 overflow-y-scroll flex justify-center items-center w-full ">
+            <div class="relative flex flex-col justify-between overflow-hidden cursor-pointer w-full object-cover object-center rounded-lg shadow-md h-64 my-2" style="background-color: slategrey ">
+                <div class="relative flex flex-row items-end  h-72 w-full ">
+                    <div class="absolute right-0 top-0 m-2">
+                    </div>
+                    <div class="p-5 rounded-lg  flex flex-col w-full z-10 ">
+                        <h4 class="mt-1 text-white text-xl font-semibold  leading-tight truncate">Job Example 1
+                        </h4>
+                        <div class="flex pt-4  text-sm text-gray-300">
+                            <div class="flex items-center mr-auto">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="relative flex flex-col justify-between overflow-hidden cursor-pointer w-full object-cover object-center rounded-lg shadow-md h-64 my-2" style="background-color: slategrey ">
+                <div class="relative flex flex-row items-end  h-72 w-full ">
+                    <div class="absolute right-0 top-0 m-2">
+                    </div>
+                    <div class="p-5 rounded-lg  flex flex-col w-full z-10 ">
+                        <h4 class="mt-1 text-white text-xl font-semibold  leading-tight truncate">Job Example 2
+                        </h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h4 class="font-semibold">For you</h4>
+        <div class="grid grid-cols-1">
+            <div class="">
+                <div class="flex  bg-white shadow-md  rounded-2xl p-2">
+                    <div class="flex flex-col justify-center w-full px-2 py-1">
+                        <div class="flex justify-between items-center ">
+                            <div class="flex flex-col">
+                                <h2 class="text-sm font-medium">Job Example</h2>
+                            </div>
+                        </div>
+                        <div class="flex pt-2  text-sm text-gray-400">
+                            <div class="flex items-center mr-auto">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-yellow-500 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </x-layout>
