26458c5d351f8d74c08e2fc2ee4ea30619746995
Alex Boevski <boevskialex@gmail.com>
2024-08-20 14:02:45 +0300

    here

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
@@ -17,7 +19,9 @@ class JobFactory extends Factory
     public function definition(): array
     {
         return [
-
+            'title' => fake()->jobTitle(),
+            'work-time' => '8 hours',
+            'salary' => fake()->numberBetween(1000, 10000),
         ];
     }
 }
