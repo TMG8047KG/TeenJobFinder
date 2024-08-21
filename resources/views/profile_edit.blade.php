2fa6cd78b24483793f411f5512ddaa8723cba4d1
Kristian <krstian.stoianov07@gmail.com>
2024-08-21 16:21:17 +0300

    neshta

--- resources/views/profile_edit.blade.php
+++ resources/views/profile_edit.blade.php
@@ -8,43 +8,43 @@
                 This is some information about the user.
             </p>
         </div>
-{{--        <div class="border-t border-gray-200 px-4 py-5 sm:p-0">--}}
-{{--            <dl class="sm:divide-y sm:divide-gray-200">--}}
-{{--                <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">--}}
-{{--                    <dt class="text-sm font-medium text-gray-500">--}}
-{{--                        Full name--}}
-{{--                    </dt>--}}
-{{--                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">--}}
-{{--                        John Doe--}}
-{{--                    </dd>--}}
-{{--                </div>--}}
-{{--                <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">--}}
-{{--                    <dt class="text-sm font-medium text-gray-500">--}}
-{{--                        Email address--}}
-{{--                    </dt>--}}
-{{--                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">--}}
-{{--                        johndoe@example.com--}}
-{{--                    </dd>--}}
-{{--                </div>--}}
-{{--                <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">--}}
-{{--                    <dt class="text-sm font-medium text-gray-500">--}}
-{{--                        Phone number--}}
-{{--                    </dt>--}}
-{{--                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">--}}
-{{--                        (123) 456-7890--}}
-{{--                    </dd>--}}
-{{--                </div>--}}
-{{--                <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">--}}
-{{--                    <dt class="text-sm font-medium text-gray-500">--}}
-{{--                        Address--}}
-{{--                    </dt>--}}
-{{--                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">--}}
-{{--                        123 Main St<br>--}}
-{{--                        Anytown, USA 12345--}}
-{{--                    </dd>--}}
-{{--                </div>--}}
-{{--            </dl>--}}
-{{--        </div>--}}
+        <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
+            <dl class="sm:divide-y sm:divide-gray-200">
+                <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">
+                        Full name
+                    </dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        John Doe
+                    </dd>
+                </div>
+                <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">
+                        Email address
+                    </dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        johndoe@example.com
+                    </dd>
+                </div>
+                <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">
+                        Phone number
+                    </dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        (123) 456-7890
+                    </dd>
+                </div>
+                <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">
+                        Address
+                    </dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        123 Main St<br>
+                        Anytown, USA 12345
+                    </dd>
+                </div>
+            </dl>
+        </div>
     </div>
     <x-nav/>
 </x-layout>
