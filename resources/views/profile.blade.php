f32a35889bcdd73810d9247aff476b4182346b81
TMG <alex800kill@gmail.com>
2024-08-21 09:21:28 +0200

    Fixed Logout button

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,10 +1,11 @@
 <x-layout>
     <x-nav/>
     <div class="bg-gray-500 h-screen">
-        <div class="relative h-full">
+        <div class="h-full">
             <div class="text-center font-semibold text-2xl py-3">
                 <p>Dashboard</p>
                 <form method="post" action="/profile/logout">
+                    @csrf
                     <button type="submit">Logout</button>
                 </form>
             </div>
