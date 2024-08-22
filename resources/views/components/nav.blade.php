d033074ae0c5004bda3a20d8fd481a3416646fd8
lookingforout <spycrabdad@gmail.com>
2024-08-22 12:04:08 +0300

    Small nav bar fix

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -17,7 +17,12 @@
                         </x-nav-link>
 
                         <x-nav-link route="post.options">
-                            <svg class="h-8 w-8 text-gray-500" viewBox="0 0 24 24" fill="{{ request()->routeIs('post.options') ? 'currentColor' : 'none' }}" stroke="currentColor">
+                            <svg
+                                class="h-8 w-8"
+                                viewBox="0 0 24 24"
+                                fill="{{ request()->routeIs('post.options') ? 'none' : 'gray' }}"
+                                stroke="gray"
+                                stroke-width="{{ request()->routeIs('post.options') ? '2.5' : '1.5' }}">
                                 <path d="M12 4v16m-8-8h16" />
                             </svg>
                         </x-nav-link>
@@ -29,7 +34,6 @@
                                 <path d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                             </svg>
                         </x-nav-link>
-
 
                         <x-nav-link route="profile">
                             <svg class="h-8 w-8 text-gray-500" viewBox="0 0 24 24" fill="{{ request()->routeIs('profile') ? 'currentColor' : 'none' }}" stroke="currentColor">
