81b5a4bfb0464eb52ee5e3f167a5daa86634fca7
lookingforout <spycrabdad@gmail.com>
2024-08-22 15:08:30 +0300

    Job Seek Create Small Design fix

--- resources/views/create/user.blade.php
+++ resources/views/create/user.blade.php
@@ -1,28 +1,20 @@
 <x-layout>
-    <x-nav/>
-    <div class="h-screen bg-gray-800">
-        <form class="h-full mx-auto content-center px-2 font-semibold">
-            <div class="form-group mb-2">
-                <x-form.label for="name">Tittle</x-form.label>
-                <x-form.input name="name" placeholder="Summer job"/>
-                <x-form.error name="name"/>
+    <div class="min-h-screen flex items-center justify-center w-full">
+        <div class="w-full max-w-md bg-white border border-white p-4">
+            <div class="text-center font-bold text-xl mb-4 text-gray-800">New Post</div>
+            <div class="flex flex-col text-gray-800">
+                <p>Tittle</p>
+                <input class="bg-gray-100 border border-gray-300 p-2 mb-4 outline-none w-full" spellcheck="false" placeholder="" type="text">
+                <p>Contact</p>
+                <input class="bg-gray-100 border border-gray-300 p-2 mb-4 outline-none w-full" spellcheck="false" placeholder="Email/Phone Number" type="text">
+                <p>Description</p>
+                <textarea class="bg-gray-100 p-3 h-48 border border-gray-300 outline-none w-full" spellcheck="false" placeholder="Describe what job you're looking for specifically"></textarea>
+                <div class="flex justify-center text-gray-500 m-2">
+                </div>
+                <div class="flex justify-center mt-4">
+                    <div class="btn border border-gray-500 shadow-md p-2 px-4 font-semibold cursor-pointer text-white bg-gray-500">Post</div>
+                </div>
             </div>
-            <div class="form-group mb-2">
-                <x-form.label for="email">Email</x-form.label>
-                <x-form.input type="email" name="email" placeholder="example@mail.com"/>
-                <x-form.error name="email"/>
-            </div>
-            <div class="form-group mb-2">
-                <x-form.label for="phone">Phone number</x-form.label>
-                <x-form.input type="phone" name="phone"/>
-                <x-form.error name="phone"/>
-            </div>
-            <div class="form-group mb-2">
-                <x-form.label for="description">Description</x-form.label>
-                <x-form.textarea name="description" row="6" placeholder="Some short information about your interest"/>
-                <x-form.error name="description"/>
-            </div>
-            <x-form.button>Submit</x-form.button>
-        </form>
+        </div>
     </div>
 </x-layout>
