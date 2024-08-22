5a3ba4d0d72b9e42aafb939a14ae6ec339fcc575
lookingforout <spycrabdad@gmail.com>
2024-08-22 14:36:50 +0300

    Job Seek Base (used the company base, both will change)

--- resources/views/create/user.blade.php
+++ resources/views/create/user.blade.php
@@ -1,4 +1,28 @@
 <x-layout>
     <x-nav/>
-    Gosho
+    <div class="h-screen bg-gray-800">
+        <form class="h-full mx-auto content-center px-2 font-semibold">
+            <div class="form-group mb-2">
+                <x-form.label for="name">Tittle</x-form.label>
+                <x-form.input name="name" placeholder="Summer job"/>
+                <x-form.error name="name"/>
+            </div>
+            <div class="form-group mb-2">
+                <x-form.label for="email">Email</x-form.label>
+                <x-form.input type="email" name="email" placeholder="example@mail.com"/>
+                <x-form.error name="email"/>
+            </div>
+            <div class="form-group mb-2">
+                <x-form.label for="phone">Phone number</x-form.label>
+                <x-form.input type="phone" name="phone"/>
+                <x-form.error name="phone"/>
+            </div>
+            <div class="form-group mb-2">
+                <x-form.label for="description">Description</x-form.label>
+                <x-form.textarea name="description" row="6" placeholder="Some short information about your interest"/>
+                <x-form.error name="description"/>
+            </div>
+            <x-form.button>Submit</x-form.button>
+        </form>
+    </div>
 </x-layout>
