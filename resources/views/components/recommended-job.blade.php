8da593cf3b1d40b9fb1c56f566a51f5d89a36401
lookingforout <spycrabdad@gmail.com>
2024-08-28 16:00:32 +0300

    Main Page stuff and dark mode guugdhsfhdshf

--- resources/views/components/recommended-job.blade.php
+++ resources/views/components/recommended-job.blade.php
@@ -6,18 +6,18 @@
     'salary'
 ])
 
-<div class="min-w-[200px] bg-white shadow-md rounded-xl p-4">
+<div class="min-w-[200px] bg-white dark:bg-gray-800 shadow-md rounded-xl p-4">
     <a href="{{ route('post.show', $id) }}">
         <div class=" relative flex justify-between overflow-hidden cursor-pointer w-full object-cover object-center rounded-lg shadow-md h-64" style="background-color: slategrey ">
-            <div class="bg-violet-600 relative flex flex-row items-start h-full w-full ">
+            <div class="bg-violet-600 dark:bg-violet-800 relative flex flex-row items-start h-full w-full ">
                 <div class="p-5 rounded-lg w-full z-10">
-                    <h4 class="mt-1 text-white text-xl font-semibold">{{ $title }}</h4>
+                    <h4 class="mt-1 text-violet-900 dark:text-gray-900 text-xl font-semibold">{{ $title }}</h4>
                     <div class="mb-4">
                         <div class="text-lg font-bold">
                             {{ $name }}
                         </div>
                     </div>
-                    <div class="text-sm text-violet-200">
+                    <div class="text-sm text-violet-200 dark:text-violet-400">
                         <p><strong>Work-time:</strong></p>
                         <p>{{ $work_time }} hours/day</p>
                         <p><strong>Salary:</strong></p>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,23 @@
 <x-layout>
-    <div x-data="{ filters: false }" class="flex flex-col h-screen max-h-screen overflow-y-auto bg-gradient-to-r from-violet-400 via-violet-600 to-violet-500">
+    <div x-data="{ filters: false }" class="flex flex-col h-screen max-h-screen overflow-y-auto bg-gradient-to-r from-violet-600 dark:from-violet-900 via-violet-600 dark:via-violet-800 to-violet-500 dark:to-violet-700 ">
 
         <!-- Header Section -->
         <div class="px-3 pt-6 pb-3 rounded-lg flex items-center space-x-2 w-full">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white" viewBox="-4 -1 20 20" fill="none" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white dark:text-gray-900" viewBox="-4 -1 20 20" fill="none" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.5 3V2.5C5.5 1.39543 6.39543 0.5 7.5 0.5C8.60457 0.5 9.5 1.39543 9.5 2.5V3M0.5 11.5H14.5M1.5 3.5H13.5C14.0523 3.5 14.5 3.94772 14.5 4.5V13.5C14.5 14.0523 14.0523 14.5 13.5 14.5H1.5C0.947716 14.5 0.5 14.0523 0.5 13.5V4.5C0.5 3.94771 0.947715 3.5 1.5 3.5Z" />
             </svg>
-            <h4 class="text-white text-2xl font-semibold leading-tight truncate">Find Your Job</h4>
+            <h4 class="text-white dark:text-gray-900 text-2xl font-semibold leading-tight truncate">Find Your Job</h4>
         </div>
 
         <!-- Search and Filters -->
         <div class="flex items-center justify-between mt-3 px-3 z-10 space-x-2">
             <div class="relative w-full">
                 <div class="absolute inset-y-0 end-0 flex items-center pointer-events-none pr-3">
-                    <svg class="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg class="h-5 w-5 text-gray-400 dark:text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                     </svg>
                 </div>
-                <input type="text" class="bg-white shadow-lg rounded-xl border-0 p-3 w-full text-gray-700 focus:ring-violet-700" placeholder="Search for a job...">
+                <input type="text" class="bg-white dark:bg-gray-800 shadow-lg rounded-xl border-0 p-3 w-full text-gray-700 dark:text-gray-600 focus:ring-violet-700" placeholder="Search for a job...">
             </div>
 {{--            <button class="bg-violet-700 text-white rounded-xl px-4 py-2 shadow-md hover:bg-violet-600 transition duration-200" @click="filters = !filters" type="button">--}}
 {{--                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">--}}
@@ -28,7 +28,7 @@
 
         <!-- Recommended Jobs Section -->
         <div class="p-3 space-y-4 z-0">
-            <h4 class="font-semibold text-white px-2">Recommended Jobs</h4>
+            <h4 class="font-semibold text-white dark:text-gray-900 px-2">Recommended Jobs</h4>
             <div class="p-3 space-y-4 z-0">
                 <div class="flex space-x-4 overflow-x-auto w-full shadow ">
                     @foreach($posts as $post)
@@ -40,9 +40,9 @@
         </div>
 
         <!-- "For You" Section -->
-        <h4 class="font-semibold text-white px-3">For You</h4>
+        <h4 class="font-semibold text-white dark:text-gray-900 px-3">For You</h4>
         <div class="grid grid-cols-1 gap-4 px-3 pb-6">
-            <div class="bg-white shadow-md rounded-2xl p-4">
+            <div class="bg-white dark:bg-gray-800 shadow-md rounded-2xl p-4">
                 <div class="flex flex-col justify-center w-full">
                     <h2 class="text-sm font-medium text-violet-600">Job Example</h2>
                     <p class="text-xs text-gray-500 mt-1">Job Description</p>
