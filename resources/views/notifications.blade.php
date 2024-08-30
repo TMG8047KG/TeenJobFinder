a302e99ca56016e95c116a1165dee0aae3f4f800
TMG <alex800kill@gmail.com>
2024-08-30 13:40:12 +0200

    Notifications scroll fix

--- resources/views/notifications.blade.php
+++ resources/views/notifications.blade.php
@@ -1,7 +1,7 @@
 <x-layout class="bg-white dark:bg-gray-900">
     <div class="h-screen bg-white dark:bg-gray-900 inset-0 w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] dark:bg-[radial-gradient(#3a3b3d_1px,transparent_1px)] [background-size:16px_16px]">
         <div class="relative">
-            <div class="absolute top-0 left-0 bg-white dark:bg-gray-800 w-full border-b border-purple-900 drop-shadow-lg">
+            <div class="sticky top-0 left-0 bg-white dark:bg-gray-800 w-full border-b border-purple-900 drop-shadow-lg">
                 <div class="p-3 inline-flex">
                     <div class="rounded-full h-10 w-10">
                         <img src="{{ auth()->user()->photo_url }}">
@@ -11,15 +11,15 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="pt-16 pb-12 h-full">
-            <div class="h-full px-2 pt-2 space-y-2">
-                @if($notifications->count() <= 0)
-                    <span class="text-gray-600 font-semibold text-xl">You don't have any notifications!</span>
-                @endif
-                @foreach($notifications as $notification)
-                    <x-notification time="{{ $notification->created_at }}" >{{ $notification->text }}</x-notification>
-                @endforeach
+            <div class="pb-12 h-full">
+                <div class="h-full px-2 py-2 space-y-2">
+                    @if($notifications->count() <= 0)
+                        <span class="text-gray-600 font-semibold text-xl">You don't have any notifications!</span>
+                    @endif
+                    @foreach($notifications as $notification)
+                        <x-notification time="{{ $notification->created_at }}" >{{ $notification->text }}</x-notification>
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
