3d85418c826f87b6bcf5c0be55cf87892b6bc0e9
Alex Boevski <boevskialex@gmail.com>
2024-10-15 12:54:43 +0300

    get

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\FavoriteController;
-use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\MarksController;
 use App\Http\Controllers\UserController;
@@ -12,14 +11,13 @@ use App\Http\Middleware\isUser;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/footer', function () {
-    return view('footer');
-})->name('footer');
+Route::get('/', function () {
+    return view('home');
+})->name('home');
 
-Route::get('/posts', [PostController::class, 'posts'])->name('jobs');
-
-Route::get('/posts/{id}', [PostController::class, 'post']);
-Route::post('/posts/{id}', [MarksController::class, 'action']);
+Route::get('/notifications', function () {
+    return view('notifications');
+})->name('notifications');
 
 Route::middleware([isUser::class])->group(function () {
     //Profile actions
@@ -28,10 +26,9 @@ Route::middleware([isUser::class])->group(function () {
     Route::get('/profile/edit', [UserController::class, 'editProfileView'])->name('profile_edit');
     Route::post('/profile/edit', [UserController::class, 'updateProfile'])->name('profile.update');
 
-    Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications');
-    Route::get('/posts/{id}/edit', [PostController::class, 'editView']);
-    Route::post('/posts/{id}/edit', [PostController::class, 'edit']);
-    Route::post('/posts/{id}/delete', [PostController::class, 'delete']);
+    Route::get('/company', function () {
+        return view('company');
+    })->name('company');
 
     //Post creation
     Route::get('/post/options', [PostController::class, 'index'])->name('post.options');
@@ -40,17 +37,13 @@ Route::middleware([isUser::class])->group(function () {
     Route::middleware([CompanyPost::class])->group(function () {
         Route::get('/post/create/company', [PostController::class, 'companyForm']);
         Route::post('/post/create/company', [PostController::class, 'listing']);
-
-        Route::get('/company/dashboard', [CompanyController::class, 'dashboard'])->name('company.dashboard');
-        Route::get('/company/edit', [CompanyController::class, 'edit'])->name('company.edit');
-        Route::post('/company/update', [CompanyController::class, 'update'])->name('company.update');
     });
     Route::middleware([CompanyMiddleware::class])->group(function () {
         Route::get('/company/create', [CompanyController::class, 'index']);
         Route::post('/company/create', [CompanyController::class, 'store']);
-
     });
 });
+
 
 Route::get('/profile/login', [UserController::class, 'loginView']);
 Route::get('/profile/register', [UserController::class, 'registerView']);
@@ -58,11 +51,5 @@ Route::post('/profile/login', [UserController::class, 'login']);
 Route::post('/profile/register', [UserController::class, 'register'])->name('register');
 
 Route::get('/posts', [PostController::class, 'posts'])->name('jobs');
-Route::post('/posts', [PostController::class, 'posts']);
 Route::get('/posts/{id}', [PostController::class, 'post'])->name('post.show');
 Route::post('/posts/{id}', [MarksController::class, 'action']);
-Route::get('/search', [PostController::class, 'searchSuggestions'])->name('search.suggestions');
-Route::get('/posts/search', [PostController::class, 'search'])->name('search');
-
-Route::get('/recommended-job', [PostController::class, 'getRecommendedJob']);
-Route::get('/', [PostController::class, 'home'])->name('home');
